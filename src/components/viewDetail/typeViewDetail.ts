23867ef2137eafeb542ea6c58a6dd9df21818f79
Niraj Bhat <niraj@fusemachines.com>
2025-05-13 20:53:23 +0545

    fix: type file renamed

---
+++ src/components/viewDetail/typeViewDetail.ts
@@ -0,0 +1,7 @@
+import type { IPokemonDetail } from '../../types/pokemon';
+
+export type IPokemonTypes = IPokemonDetail['types'];
+
+export type IPokemonStats = IPokemonDetail['stats'];
+
+export type IPokemonAbilities = IPokemonDetail['abilities'];
--- src/components/viewDetail/viewDetails.ts
+++
@@ -1,3 +0,0 @@
-import type { IPokemonDetail } from '../../types/pokemon';
-
-export type IStats = IPokemonDetail['stats'];
