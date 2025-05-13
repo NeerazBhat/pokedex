75f85c577402f6141dd40b00c3f446338f605cfa
Niraj Bhat <niraj@fusemachines.com>
2025-05-13 22:33:20 +0545

    style: random color generator array

---
+++ src/data/pokemonMovesColor.ts
@@ -0,0 +1,12 @@
+export const pokemonMovesColor = [
+  'green.200',
+  'red.200',
+  'orange.300',
+  'green.300',
+  'yellow.400',
+  'teal.600',
+  'cyan.400',
+  'purple.400',
+  'gray.200',
+  'orange.500',
+];
