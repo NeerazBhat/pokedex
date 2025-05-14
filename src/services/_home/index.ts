8ca59905769eb445a6ef752daf6e53ff2e59f8fb
Niraj Bhat <niraj@fusemachines.com>
2025-05-14 17:54:19 +0545

    feat: fetched evoultion by id and species by name

--- src/services/_home/index.ts
+++ src/services/_home/index.ts
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import type { IEvolutionDetail } from '../../types/evolution';
+import type { ISpeciesDetail } from '../../types/species';
 
 export async function fetchPokemons(offset: number, limit: number) {
   const { data } = await axios.get(
@@ -9,5 +11,21 @@ export async function fetchPokemons(offset: number, limit: number) {
 
 export async function fetchPokemonDetail(name: string) {
   const { data } = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`);
+  return data;
+}
+
+export async function fetchPokemonSpecies(
+  name: string
+): Promise<ISpeciesDetail> {
+  const { data } = await axios.get<ISpeciesDetail>(
+    `https://pokeapi.co/api/v2/pokemon-species/${name}`
+  );
+  return data;
+}
+
+export async function fetchPokemonEvolution(
+  url: string
+): Promise<IEvolutionDetail> {
+  const { data } = await axios.get<IEvolutionDetail>(`${url}`);
   return data;
 }
