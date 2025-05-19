e82cb1be1c7fb93aa104f3411e91c94af14f080e
Niraj Bhat <niraj@fusemachines.com>
2025-05-19 17:06:38 +0545

    feat: my favouties add and view in page feat added

--- src/pages/MyFavourites.tsx
+++ src/pages/MyFavourites.tsx
@@ -1,30 +1,65 @@
-import { Container } from '@chakra-ui/react';
-import { useQuery } from '@tanstack/react-query';
+import { Container, Heading, SimpleGrid } from '@chakra-ui/react';
+import { useQueries, useQuery } from '@tanstack/react-query';
 import ErrorMessage from '../components/common/ErrorMessage';
 import Loader from '../components/common/Loader';
-import { fetchFavPokemons } from '../services/_home';
+import { fetchFavPokemons } from '../services/favourites';
+import type { IFavPokemonData } from '../types/favourites';
+import { fetchPokemonDetail } from '../services/home';
+import PokemonCard from '../components/home/pokemonCard/PokemonCard';
+import type { IPokemonDetail } from '../types/pokemon';
 
 const MyFavourites = () => {
   const {
-    isLoading,
-    isError,
+    isLoading: isFavPokemonListLoading,
+    isError: isFavPokemonListError,
     data: favPokemonList,
-  } = useQuery({
+  } = useQuery<IFavPokemonData[]>({
     queryKey: ['favPokemon'],
     queryFn: fetchFavPokemons,
   });
 
-  if (isLoading) {
+  const results = favPokemonList || [];
+
+  const favPokemonListQueries = useQueries({
+    queries: results.map((pokemon) => ({
+      queryKey: ['pokemon', pokemon.name],
+      queryFn: () => fetchPokemonDetail(pokemon.name),
+      staleTime: 30000,
+    })),
+  });
+
+  const isPokemonDetailLoading = favPokemonListQueries.some(
+    (data) => data.isLoading
+  );
+
+  const isPokemonDetailError = favPokemonListQueries.some(
+    (data) => data.isError
+  );
+
+  if (isFavPokemonListLoading || isPokemonDetailLoading) {
     return <Loader />;
   }
 
-  if (isError) {
+  if (isFavPokemonListError || isPokemonDetailError) {
     return <ErrorMessage message="Error something went wrong" />;
   }
 
-  console.log(favPokemonList);
+  const loadedPokemonList = favPokemonListQueries?.map(
+    (query) => query.data as IPokemonDetail
+  );
 
-  return <Container maxW="7xl">MyFavourtites</Container>;
+  return (
+    <Container maxW="7xl">
+      <Heading as="h2" my={6}>
+        My Favourites
+      </Heading>
+      <SimpleGrid columns={5} spacing={4}>
+        {loadedPokemonList.map((pokemon) => (
+          <PokemonCard key={pokemon.id} pokemonName={pokemon.name} />
+        ))}
+      </SimpleGrid>
+    </Container>
+  );
 };
 
 export default MyFavourites;
