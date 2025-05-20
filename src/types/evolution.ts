c0e5f5a9889149c2a9b0947c7165c1cb9eb6d838
Niraj Bhat <niraj@fusemachines.com>
2025-05-20 19:56:49 +0545

    feat: url added on evolution type

--- src/types/evolution.ts
+++ src/types/evolution.ts
@@ -2,14 +2,17 @@ export interface IEvolutionDetail {
   chain: {
     species: {
       name: string;
+      url: string;
     };
     evolves_to: Array<{
       species: {
         name: string;
+        url: string;
       };
       evolves_to: Array<{
         species: {
           name: string;
+          url: string;
         };
       }>;
     }>;
