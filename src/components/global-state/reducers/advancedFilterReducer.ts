2fc31dfdf32e9dd73f0efbf0076d5a42a0a52747
Niraj Bhat <niraj@fusemachines.com>
2025-05-21 20:24:44 +0545

    feat: advanced filter reducer

---
+++ src/components/global-state/reducers/advancedFilterReducer.ts
@@ -0,0 +1,40 @@
+import {
+  FilterActionTypes,
+  type filterActions,
+} from '../actions/advancedFilterActions';
+
+export const INITIAL_STATE = {
+  draft: { type: '', habitat: '', classification: '' },
+  applied: { type: '', habitat: '', classification: '' },
+};
+
+export interface IFilterState {
+  draft: { type: string; habitat: string; classification: string };
+  applied: { type: string; habitat: string; classification: string };
+}
+
+export const advancedFilterReducer = (
+  state: IFilterState,
+  action: filterActions
+) => {
+  switch (action.type) {
+    case FilterActionTypes.SET_TYPE:
+      return { ...state, draft: { ...state.draft, type: action.payload } };
+    case FilterActionTypes.SET_HABITAT:
+      return { ...state, draft: { ...state.draft, habitat: action.payload } };
+    case FilterActionTypes.SET_CLASSIFICATION:
+      return {
+        ...state,
+        draft: { ...state.draft, classification: action.payload },
+      };
+    case FilterActionTypes.APPLY_FILTERS:
+      return { ...state, applied: { ...state.draft } };
+    case FilterActionTypes.CLEAR_FILTERS:
+      return {
+        draft: { type: '', habitat: '', classification: '' },
+        applied: { type: '', habitat: '', classification: '' },
+      };
+    default:
+      return state;
+  }
+};
