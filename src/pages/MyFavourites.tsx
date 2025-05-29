c8320e54cd98b1b3fe23755a3046a0ef52778738
Niraj Bhat <niraj@fusemachines.com>
2025-05-29 20:55:25 +0545

    fix: removed unnecessary fetch or pokemon details in myfav

--- src/pages/MyFavourites.tsx
+++ src/pages/MyFavourites.tsx
@@ -7,9 +7,7 @@ import {
 } from '@chakra-ui/react';
 import ErrorMessage from '../components/common/ErrorMessage';
 import Loader from '../components/common/Loader';
-import type { IPokemonDetail } from '../types/pokemon';
 import { useFavPokemonList } from '../hooks/useFavPokemonList';
-import { usePokemonDetail } from '../hooks/usePokemonDetail';
 import { Link } from 'react-router';
 import { PAGE_URLS } from '../lib/routes';
 import PokemonCard from '../components/common/PokemonCard';
@@ -23,29 +21,15 @@ const MyFavourites = () => {
 
   const results = favPokemonList || [];
 
-  const favPokemonListQueries = usePokemonDetail(results);
-
-  const isPokemonDetailLoading = favPokemonListQueries.some(
-    (data) => data.isLoading
-  );
-
-  const isPokemonDetailError = favPokemonListQueries.some(
-    (data) => data.isError
-  );
-
-  if (isFavPokemonListLoading || isPokemonDetailLoading) {
+  if (isFavPokemonListLoading) {
     return <Loader />;
   }
 
-  if (isFavPokemonListError || isPokemonDetailError) {
+  if (isFavPokemonListError) {
     return <ErrorMessage message="Error something went wrong" />;
   }
 
-  const loadedPokemonList = favPokemonListQueries?.map(
-    (query) => query.data as IPokemonDetail
-  );
-
-  if (results.length < 1) {
+  if (results?.length < 1) {
     return (
       <VStack minH="90dvh" justifyContent="center">
         <Heading mb={4}>Your Favourites list is empty right now</Heading>
@@ -62,7 +46,7 @@ const MyFavourites = () => {
         My Favourites
       </Heading>
       <SimpleGrid columns={5} spacing={4}>
-        {loadedPokemonList.map((pokemon) => (
+        {results.map((pokemon) => (
           <PokemonCard
             key={pokemon.id}
             pokemonName={pokemon.name}
