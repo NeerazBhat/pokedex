b1e9a82a2497bd2654591ce7a7f06c0fe99b51e7
Niraj Bhat <niraj@fusemachines.com>
2025-06-19 20:31:51 +0545

    feat: auth0 implementation

--- src/main.tsx
+++ src/main.tsx
@@ -4,16 +4,26 @@ import App from './App.tsx';
 import { BrowserRouter } from 'react-router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { Auth0Provider } from '@auth0/auth0-react';
+
+const domain: string = import.meta.env.VITE_AUTH0_DOMAIN;
+const clientId: string = import.meta.env.VITE_AUTH0_CLIENT_ID;
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-      <ReactQueryDevtools initialIsOpen />
-    </QueryClientProvider>
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      authorizationParams={{ redirect_uri: window.location.origin }}
+    >
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+        <ReactQueryDevtools initialIsOpen />
+      </QueryClientProvider>
+    </Auth0Provider>
   </StrictMode>
 );
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+interface ImportMetaEnv {
+  readonly VITE_AUTH0_DOMAIN: string;
+  readonly VITE_AUTH0_CLIENT_ID: string;
+  // Add more if needed
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
